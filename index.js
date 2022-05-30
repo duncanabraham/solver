a4050ed54655b00aec11556a6d2d5e13acc42391
Duncan Abraham <dunc@buzzler.co.uk>
2022-05-30 07:02:32 +0100

    adding maze

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const { data: maze } = require('./config')
+const { data, mazeSettings } = require('./config')
 const Solver = require('./lib/solver')
+const Maze = require('./lib/maze')
+
 const thisSolver = new Solver()
+const thisMaze = new Maze({
+  width: 10,
+  height: 10,
+  maze: data
+})
 
 const startPos = [5, 3]
 const endPos = [10,0]
 
-const route = thisSolver.solve(maze, startPos, endPos)
+
+const route = thisSolver.solve(thisMaze, startPos, endPos)
 
 console.log(route)
